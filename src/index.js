785207b0275e9a0c63f7e393e8c317be883f58f8
Memo Doring <memo@doring.me>
2015-11-24 20:14:37 -0800

    Added first version of 'speech assets' and node.js 'src'

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//var APP_ID = "amzn1.echo-sdk-ams.app.b14cd08a-9217-4c9b-9960-40c9560b6e9e";
+
+var http       	= require('http')
+  , AlexaSkill 	= require('./AlexaSkill')
+  , APP_ID     	= "amzn1.echo-sdk-ams.app.b14cd08a-9217-4c9b-9960-40c9560b6e9e"
+  , API_KEY 	= 'b64770ae6b5c6a2f0aef95bcff641649a77cf997'
+  , username 	= 'memodoring';
---
+++ src/node_sample.js
@@ -0,0 +1,31 @@
+
+/*
+ * This requires: restler
+ * To install, type 'npm install restler'
+ * Tested with node.js v0.6.14
+ */
+
+var moment = require('moment');
+var restclient = require('restler');
+
+var fxml_url = 'http://flightxml.flightaware.com/json/FlightXML2/';
+var username = 'memodoring';
+var apiKey = 'b64770ae6b5c6a2f0aef95bcff641649a77cf997';
+
+
+restclient.get(fxml_url + 'FlightInfo', {
+  username: username,
+  password: apiKey,
+  query:{ident:'ASA746', howMany: 3}
+}).on('success', function(result,response){
+  //console.log(util.inspect(result, true, null));
+  var flights = result.FlightInfoResult.flights;
+  for (i in flights) {
+    var timeInSec= flights[i].estimatedarrivaltime;
+    date= moment.unix(timeInSec).format("LLLL");
+    
+    console.log(flights[i].ident +" arrival: "+ date);
+  };
+});
+//{ FlightInfoResult: { next_offset: 1, flights: [ [Object] ] } }
+//estimatedarrivaltime: 1448411982,
